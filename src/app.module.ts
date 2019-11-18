411082ad1e6c3929584644c714b207e7853fd995
唐金健 <1271115696@qq.com>
2019-11-18 21:50:05 +0800

    refactor: 数据库连接，修改为认证连接

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,34 @@ import { AuthModule } from './feature/auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TodosModule } from './feature/todos/todos.module';
 
-let mongoHost: string;
-if (process.env.MONGO_HOST) {
-  mongoHost = process.env.MONGO_HOST;
-} else {
-  mongoHost = 'localhost';
+/**
+ * 如果在开发模式，则从本地读取数据库认证。
+ */
+declare const module: any;
+let MongoAuth: {
+  MONGO_INITDB_ROOT_USERNAME: string;
+  MONGO_INITDB_ROOT_PASSWORD: string;
+};
+if (module.hot) {
+  // tslint:disable-next-line: no-var-requires
+  MongoAuth = require('./mongo-auth.json');
 }
+
+const {
+  MONGO_HOST = 'localhost',
+  MONGO_PORT = '27017',
+  MONGO_DB = 'todo',
+  MONGO_INITDB_ROOT_USERNAME = MongoAuth.MONGO_INITDB_ROOT_USERNAME,
+  MONGO_INITDB_ROOT_PASSWORD = MongoAuth.MONGO_INITDB_ROOT_PASSWORD,
+} = process.env;
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
-    MongooseModule.forRoot(`mongodb://${mongoHost}/todo`),
+    MongooseModule.forRoot(
+      `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`,
+    ),
     TodosModule,
   ],
   controllers: [AppController],
